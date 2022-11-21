7c6f73408da1aaaf38d4476eb82695ef1eefe1e4
Ismael <ismael@MacBook-Air-de-Ismael.local>
2022-11-21 13:32:47 -0600

    update-loding-screen

--- src/Components/LoadingScreen.jsx
+++ src/Components/LoadingScreen.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+
 function LoadingScreen() {
   return (
     <div className='divisLoading'>
-      <h1 className='text-center'>
-        loading .....
-      </h1>
+      <div id="preloader">
+        <div id="loader">
+          
+        </div>
+      </div>
     </div>
     
   )
